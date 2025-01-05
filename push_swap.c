ce099037200eae4db579fcec662767dadc24792e
Shiyun <serene.hoshiyun@gmail.com>
2025-01-05 16:50:52 +0800

    Formatted the code

--- push_swap.c
+++ push_swap.c
@@ -6,17 +6,12 @@
 /*   By: shiyun <shiyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 15:19:18 by shiyun            #+#    #+#             */
-/*   Updated: 2025/01/05 15:14:06 by shiyun           ###   ########.fr       */
+/*   Updated: 2025/01/05 16:48:00 by shiyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "./libft/libft.h"
 # include <limits.h>
-
-void	del_int(int content)
-{
-	(void)content;
-}
 
 int		handle_error(int *array, t_list **node)
 {
@@ -28,7 +23,7 @@ int		handle_error(int *array, t_list **node)
 	return (0);
 }
 
-int	has_duplicate(int *array, int size)
+int		has_duplicate(int *array, int size)
 {
 	int	i;
 		
@@ -42,41 +37,57 @@ int	has_duplicate(int *array, int size)
 	return (0);
 }
 
+int		validate_input(long num, char *arg, int *array, int index)
+{
+	if (num == 0 && (arg[0] != '0' || arg[1] != '\0'))
+		return (1);
+	if (has_duplicate(array, index))
+		return (1);
+	return (0);
+}
+int		create_linked_list(int argc, char *argv[], int **array, t_list **head)
+{
+	int		i;
+	long	num;
+	t_list	*new_node;
+
+	*array = malloc((argc - 1) * sizeof(int));
+	if (!*array)
+		return (0);
+	
+	i = 1;
+	*head = NULL;
+	while (i < argc)
+	{
+		num = ft_atol(argv[i]);
+		if (num < INT_MIN || num> INT_MAX)
+			return (0);
+		(*array)[i - 1] = (int)num;
+		new_node = ft_lstnew((int)num);
+		if (!new_node)
+			return (0);
+		ft_lstadd_front(head, new_node);
+		if (validate_input(num, argv[i], *array, i))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 /**
  * @brief: Takes an argument `stack a`, formatted as list of integers,
  *         Display list of instructions to sort `stack a` in ascending order
- *
 */
 int	main(int argc, char *argv[])
 {
-	int			i;
-	long		num;
-	int			*duplicate_check_array;
-	t_list		*head;
-	t_list		*new_node;
-	
+	int		*duplicate_check_array;
+	t_list	*head;
+
 	if (argc == 1)
 		return (0);
-	i = 1;
-	head = NULL;
-	duplicate_check_array = malloc((argc - 1) * sizeof(int));
-	if (!duplicate_check_array)
-		return (0);
-	while (i < argc)
-	{
-		num = ft_atol(argv[i]);	
-		duplicate_check_array[i-1] = (int)num;
-		new_node = ft_lstnew((int)num);
-		if (!new_node)
-			return (handle_error(duplicate_check_array, &head));
-		ft_lstadd_front(&head, new_node);
-		if ((num == 0 && (argv[i][0] != '0' || argv[i][1] != '\0')) ||
-            (num < INT_MIN || num > INT_MAX) ||
-            (has_duplicate(duplicate_check_array, i)))
-            return handle_error(duplicate_check_array, &head);
-		i++;
-	}
+	if (!create_linked_list(argc, argv, &duplicate_check_array, &head))
+		return handle_error(duplicate_check_array, &head);
 	free(duplicate_check_array);
+	//TODO: Sort the stack
 	ft_lstclear(&head, del_int);
 	return (1);
 }
