5a176210c4e4e9e48616fd3a7346cd545c4fc74d
Shiyun <serene.hoshiyun@gmail.com>
2025-01-15 21:27:11 +0800

    Refactored algorithm for 3 inputs

--- sort_stack.c
+++ sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 16:58:47 by shiyun            #+#    #+#             */
-/*   Updated: 2025/01/15 21:17:18 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2025/01/15 21:25:50 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,83 +28,122 @@ void	sort_small_input(t_list **stack_a, int size)
 	}
 	else
 	{
-		int	a; //first number
+		int	a;
 		int	b;
-		int	c; //third number
+		int	c;
 		
 		a = (*stack_a)->content;
 		b = (*stack_a)->next->content;
 		c = (*stack_a)->next->next->content;
-
-/**
- * Scenarios (ascending)
- * a, b, c (V)
- * a, c, b (V)
- * b, c, a (V)
- * b, a, c (V)
- * c, a, b (V)
- * c, b, a (V)
- */
-/*
-c => 
-a
-b
-*/
 		if (a < b && b < c)	// a, b, c
 			return ;
-		if (a > b && a < c)
+		if (a > b)
 		{
-			/**
-			 * a  b
-			 * b  a
-			 * c  c
-			 */
-			sa(stack_a);
-		}
-		if (a > b && a > c)
-		{
-			/**
-			 * a  b  c
-			 * b  a	 b		 		   	
-			 * c  c	 a		 
-			*/
-			if (b > c)
-			{
+			if (a < c)
 				sa(stack_a);
-				rra(stack_a); // c, b, a
+			else if (a > c)
+			{
+				if (b > c)
+				{
+					sa(stack_a);
+					rra(stack_a); //c, b, a
+				}
+				else if (b < c)
+					ra(stack_a); //b, c, a
 			}
-			/**
-			 * a b
-			 * b c
-			 * c a
-			 */
-			if (b < c) // b, c, a
-				ra(stack_a); 
 		}
 		else if (a < b && b > c)
 		{
-			if (a > c) //c, a, b
-			{
-				/**
-				 * a
-				 * b
-				 * c
-				 */
-				rra(stack_a);
-			}
-			else if (a < c) //a, c, b
-			{
-				/**
-				 * a c a
-				 * b a c
-				 * c b b
-				 */
-				rra(stack_a);				
+			rra(stack_a); // c, a, b
+			if (a < c) // a, c, b
 				sa(stack_a);
-			}
 		}
 	}
 }
+
+// void	sort_small_input(t_list **stack_a, int size)
+// {
+// 	if (size == 1)
+// 		return (0);
+// 	else if (size == 2)
+// 	{
+// 		if ((*stack_a)->content > (*stack_a)->next->content)
+// 			sa(stack_a);	//swap first two elements in stack_a
+// 	}
+// 	else
+// 	{
+// 		int	a; //first number
+// 		int	b;
+// 		int	c; //third number
+		
+// 		a = (*stack_a)->content;
+// 		b = (*stack_a)->next->content;
+// 		c = (*stack_a)->next->next->content;
+
+// /**
+//  * Scenarios (ascending)
+//  * a, b, c (V)
+//  * a, c, b (V)
+//  * b, c, a (V)
+//  * b, a, c (V)
+//  * c, a, b (V)
+//  * c, b, a (V)
+//  */
+// 		if (a < b && b < c)	// a, b, c
+// 			return ;
+// 		if (a > b && a < c)
+// 		{
+// 			/**
+// 			 * a  b
+// 			 * b  a
+// 			 * c  c
+// 			 */
+// 			sa(stack_a);
+// 		}
+// 		if (a > b && a > c)
+// 		{
+// 			/**
+// 			 * a  b  c
+// 			 * b  a	 b		 		   	
+// 			 * c  c	 a		 
+// 			*/
+// 			if (b > c)
+// 			{
+// 				sa(stack_a);
+// 				rra(stack_a); // c, b, a
+// 			}
+// 			/**
+// 			 * a b
+// 			 * b c
+// 			 * c a
+// 			 */
+// 			if (b < c) // b, c, a
+// 				ra(stack_a); 
+// 		}
+// 		else if (a < b && b > c)
+// 		{
+// 			if (a > c) //c, a, b
+// 			{
+// 				/**
+// 				 * a
+// 				 * b
+// 				 * c
+// 				 */
+// 				rra(stack_a);
+// 			}
+// 			else if (a < c) //a, c, b
+// 			{
+// 				/**
+// 				 * a c a
+// 				 * b a c
+// 				 * c b b
+// 				 */
+// 				rra(stack_a);				
+// 				sa(stack_a);
+// 			}
+// 		}
+// 	}
+// }
 
 /**
  * @brief: Sorts stack from user input
@@ -124,6 +163,7 @@ void    sort_stack(t_list **stack_a)
 	//TODO: Implement sorting algorithm for 3 inputs, 100 inputs, more than 100
 	if (size <= 3)
 		sort_small_input(stack_a, size);
+	//TODO: 3, 5(12 moves), 100, 500
 	else if (size <= 100)
 		sort_medium_input(stack_a);
 	else
