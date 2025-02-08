7d299d40e0778df72161301bd79e27b882b1857d
Shiyun <serene.hoshiyun@gmail.com>
2025-02-08 20:26:46 +0800

    Fixed segmentation fault issues! hehe

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 15:19:18 by shiyun            #+#    #+#             */
-/*   Updated: 2025/02/08 14:39:17 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2025/02/08 20:25:38 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,21 @@
 #include <limits.h>
 #include <stdlib.h>
 
-// TODO: Fix segmentation fault caused by improper handling of string input
-// ISSUE: Reassigning `argv` directly to `ft_split()` result causes undefined behavior
-// SOLUTION: Use a separate variable (e.g., `split_args`) to store split arguments instead of reassigning `argv`
-
-// TODO: Ensure proper memory management for dynamically allocated split arguments
-// ISSUE: Memory allocated by `ft_split()` is not freed, causing memory leaks
-// SOLUTION: Free each element of `split_args` and then free the array itself at the end of the program
-
-// TODO: Correctly update `argc` after splitting the string
-// ISSUE: `argc` is not updated properly when processing split arguments
-// SOLUTION: Update `argc` based on the number of elements in `split_args` using a helper function like `count_no_in_string()`
-
-// TODO: Validate and process arguments consistently
-// ISSUE: Accessing invalid memory or incorrect indices in `argv` after reassignment
-// SOLUTION: Use `split_args` for validation and processing if `argv[1]` contains spaces
-
-// TODO: Debug and test edge cases
-// ISSUE: Potential null-pointer dereferences or invalid memory access
-// SOLUTION: Add debug prints and use tools like `valgrind` to detect and fix memory errors
-
-int		count_no_in_string(char *argv[])
+int		count_no_in_array(char **array)
 {
-	int		split_arg_count;
+	int		i;
 
-	split_arg_count = 0;
-	while (argv[split_arg_count])
-		split_arg_count++;
-	return (split_arg_count);
+	i = 0;
+	while (array[i])
+		i++;
+	return (i);
 }
 
-char	**handle_string_input(char *argv[])
+char	**handle_string_input(char *arg)
 {
-	int		no_of_inputs;
 	char	**args_array;
 
-	no_of_inputs = count_no_in_string(argv);
-	*args_array = (int *)malloc((no_of_inputs - 1) * sizeof(int));
-	args_array = ft_split(argv[1], ' ');
+	args_array = ft_split(arg, ' ');
 	if (!args_array)
 		handle_error(NULL, NULL);
 	return (args_array);
@@ -67,18 +44,39 @@ int	main(int argc, char *argv[])
 	int		*duplicate_check_array;
 	t_list	*head;
 	char	**args_array;
+	char	**new_argv;
 	int		i;
 
 	if (argc == 1)
 		return (0);
+
+	args_array = NULL;
+	new_argv = NULL;
+
 	if (argc >= 2 && ft_strchr(argv[1], ' ') != NULL)
 	{
-		args_array = handle_string_input(argv);
-		argc = count_no_in_string(args_array) + 1;
-		argv = args_array;
+		args_array = handle_string_input(argv[1]);
+		int split_count = count_no_in_array(args_array);
+		int new_argc = split_count + 1;
+		
+		new_argv = (char **)malloc((new_argc + 1) * sizeof(char*));
+		if (!new_argv)
+			handle_error(NULL, NULL);
+		
+		new_argv[0] = argv[0];
+		i = 0;
+		while (i < split_count)
+		{
+			new_argv[i + 1] = args_array[i];
+			i++;
+		}
+		new_argv[new_argc] = NULL;
+
+		argc = new_argc;
+		argv = new_argv;
 	}
 	i = 1;
-	while (argv[i])
+	while (i < argc)
 	{
 		check_valid_argument(argv[i]);
 		i++;
@@ -88,8 +86,12 @@ int	main(int argc, char *argv[])
 		handle_error(duplicate_check_array, &head);
 		exit(EXIT_FAILURE);
 	}
+
 	sort_stack(&head);
+
 	ft_lstclear(&head, del_int);
+	free(duplicate_check_array);
+
 	if (args_array)
 	{
 		i = 0;
@@ -100,6 +102,8 @@ int	main(int argc, char *argv[])
 		}
 		free(args_array);
 	}
+	if (new_argv)
+		free(new_argv);
 	return (1);
 }
 
