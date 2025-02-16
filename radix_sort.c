e668cbb5c4326f296f0000ae190762231fdce017
Shiyun <serene.hoshiyun@gmail.com>
2025-02-16 19:58:45 +0800

    bug - original linked list was sorted during quicksort

--- radix_sort.c
+++ radix_sort.c
@@ -32,6 +32,72 @@ int		bits_for_highest_rank(int rank)
 	return (num_bits);
 }
 
+void print_stack(t_list *stack, char name) {
+    printf("Stack %c: ", name);
+    while (stack) {
+        ft_printf("%d ", stack->rank); // Assuming `rank` is the value you want to print
+        stack = stack->next;
+    }
+    ft_printf("\n");
+}
+
+void radix_sort(t_list **stack_a, t_list **stack_b, int size)
+{
+    int num_bits;
+    int bit;
+    int i;
+    int rank;
+
+    num_bits = bits_for_highest_rank(size - 1); // Calculate the number of bits needed
+    bit = 0;
+
+	printf("Checking rank for first node: %i position for value %i\n", (*stack_a)->rank, (*stack_a)->content);
+    while (bit < num_bits)
+    {
+        i = 0;
+        while (i < size)
+        {
+			rank = (*stack_a)->rank; // Get the rank of the top node in stack A
+
+            // Print the state of both stacks BEFORE the operation
+            printf("Before operation (bit %d, i %d):\n", bit, i);
+            print_stack(*stack_a, 'A');
+            print_stack(*stack_b, 'B');
+			
+
+            if ((rank & (1 << bit)) == 0) // If the current bit is 0
+            {
+                pb(stack_a, stack_b); // Push to stack B
+                printf("After pb:\n");
+            }
+            else // If the current bit is 1
+            {
+                ra(stack_a); // Rotate stack A
+                printf("After ra:\n");
+            }
+
+            // Print the state of both stacks
+            print_stack(*stack_a, 'A');
+            print_stack(*stack_b, 'B');
+
+            i++;
+        }
+
+        // Move all elements back from stack B to stack A
+        while (*stack_b)
+        {
+            pa(stack_a, stack_b); // Push back to stack A
+            printf("After pa:\n");
+
+            // Print the state of both stacks
+            print_stack(*stack_a, 'A');
+            print_stack(*stack_b, 'B');
+        }
+
+        bit++; // Move to the next bit
+    }
+}
+
 /**
 1) Calculate if element is closer to top or bottom.
 a) Is target element closer to the top? - ra
@@ -44,29 +110,29 @@ a) Push elements with 0 in current bit to stack_b
 b) Leave elements with 1 in current bit to stack_a
  */
 
-void	radix_sort(t_list **stack_a, t_list **stack_b, int size)
-{
-	int		num_bits;
-	int		bit;
-	int		i;
-	int		rank;
+// void	radix_sort(t_list **stack_a, t_list **stack_b, int size)
+// {
+// 	int		num_bits;
+// 	int		bit;
+// 	int		i;
+// 	int		rank;
 
-	num_bits = bits_for_highest_rank(size - 1);
-	bit = 0;
-	while (bit < num_bits)
-	{
-		i = 0;
-		while (i < size)
-		{
-			rank = (*stack_a)->rank;
-			if ((rank & (1 << bit)) == 0) //if bit == 0
-				pb(stack_a, stack_b);
-			else // if bit == 1
-				ra(stack_a);
-			i++;
-		}
-		while (*stack_b)
-			pa(stack_a, stack_b);
-		bit++;
-	}
-}
+// 	num_bits = bits_for_highest_rank(size - 1);
+// 	bit = 0;
+// 	while (bit < num_bits)
+// 	{
+// 		i = 0;
+// 		while (i < size)
+// 		{
+// 			rank = (*stack_a)->rank;
+// 			if ((rank & (1 << bit)) == 0) //if bit == 0
+// 				pb(stack_a, stack_b);
+// 			else // if bit == 1
+// 				ra(stack_a);
+// 			i++;
+// 		}
+// 		while (*stack_b)
+// 			pa(stack_a, stack_b);
+// 		bit++;
+// 	}
+// }
--- sort_stack.c
+++ sort_stack.c
@@ -31,27 +31,6 @@ int		is_sorted(t_list *stack_a)
  * @brief: Sorts stack from user input
  * @param: `stack_a` - Stack generated from user inputs
  */
-void    sort_stack(t_list **stack_a)
-{
-	int		size;
-	t_list	*stack_b;
-
-	stack_b = NULL;
-	size = ft_lstsize(*stack_a);
-	if (is_sorted(*stack_a))
-		return ;
-	if (size <= 3)
-		sort_three_elements(stack_a, size);
-	else if (size <= 5)
-		sort_five_elements(stack_a, &stack_b, size);
-	else 
-	{
-		quick_sort(stack_a);
-		radix_sort(stack_a, &stack_b, size);
-	}
-}
-
-
 // void    sort_stack(t_list **stack_a)
 // {
 // 	int		size;
@@ -59,26 +38,6 @@ void    sort_stack(t_list **stack_a)
 
 // 	stack_b = NULL;
 // 	size = ft_lstsize(*stack_a);
-	
-// 	// TODO: REMOVE LATER
-// 	ft_printf("Size: %i\n", size);
-// 	ft_printf("Before sort...\n");
-
-// 	ft_printf(">>> stack_a:\n");
-// 	t_list *org_a = *stack_a;
-// 	while (org_a)
-// 	{
-// 		ft_printf("%i\n", org_a->content);
-// 		org_a = org_a->next;
-// 	}
-
-// 	ft_printf(">>> stack_b:\n");
-// 	t_list *org_b = stack_b;
-// 	while (org_b)
-// 	{
-// 		ft_printf("%i\n", org_b->content);
-// 		org_b = org_b->next;
-// 	}
 // 	if (is_sorted(*stack_a))
 // 		return ;
 // 	if (size <= 3)
@@ -90,22 +49,63 @@ void    sort_stack(t_list **stack_a)
 // 		quick_sort(stack_a);
 // 		radix_sort(stack_a, &stack_b, size);
 // 	}
-	
-// 	// TODO: REMOVE LATER
-// 	ft_printf("\n\nCompleted sort...\n");
-// 	ft_printf(">>> stack_a:\n");
-// 	t_list *current_a = *stack_a;
-// 	while (current_a)
-// 	{
-// 		ft_printf("%i\n", current_a->content);
-// 		current_a = current_a->next;
-// 	}
-
-// 	ft_printf(">>> stack_b:\n");
-// 	t_list *current_b = stack_b;
-// 	while (current_b)
-// 	{
-// 		ft_printf("%i\n", current_b->content);
-// 		current_b = current_b->next;
-// 	}
 // }
+
+
+void    sort_stack(t_list **stack_a)
+{
+	int		size;
+	t_list	*stack_b;
+
+	stack_b = NULL;
+	size = ft_lstsize(*stack_a);
+	
+	// TODO: REMOVE LATER
+	ft_printf("Size: %i\n", size);
+	ft_printf("Before sort...\n");
+
+	ft_printf(">>> stack_a:\n");
+	t_list *org_a = *stack_a;
+	while (org_a)
+	{
+		ft_printf("%i\n", org_a->content);
+		org_a = org_a->next;
+	}
+
+	ft_printf(">>> stack_b:\n");
+	t_list *org_b = stack_b;
+	while (org_b)
+	{
+		ft_printf("%i\n", org_b->content);
+		org_b = org_b->next;
+	}
+	if (is_sorted(*stack_a))
+		return ;
+	if (size <= 3)
+		sort_three_elements(stack_a, size);
+	else if (size <= 5)
+		sort_five_elements(stack_a, &stack_b, size);
+	else 
+	{
+		quick_sort(stack_a);
+		radix_sort(stack_a, &stack_b, size);
+	}
+	
+	// TODO: REMOVE LATER
+	ft_printf("\n\nCompleted sort...\n");
+	ft_printf(">>> stack_a:\n");
+	t_list *current_a = *stack_a;
+	while (current_a)
+	{
+		ft_printf("%i\n", current_a->content);
+		current_a = current_a->next;
+	}
+
+	ft_printf(">>> stack_b:\n");
+	t_list *current_b = stack_b;
+	while (current_b)
+	{
+		ft_printf("%i\n", current_b->content);
+		current_b = current_b->next;
+	}
+}
