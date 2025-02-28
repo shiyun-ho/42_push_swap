ce5ac6449a79f3c1144806e5c81d07adbd998895
Shiyun <serene.hoshiyun@gmail.com>
2025-02-28 22:20:48 +0800

    added digit check to args

--- process_quote_utils.c
+++ process_quote_utils.c
@@ -6,7 +6,7 @@
 /*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 21:52:39 by hshi-yun          #+#    #+#             */
-/*   Updated: 2025/02/12 21:57:15 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2025/02/28 22:19:53 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,41 @@ int	count_no_in_quoted_arg(char *argv[])
 	return (total_no);
 }
 
+int		digit_check(int argc, char ***argv)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i <= argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (ft_isdigit(argv[i][j]) == 0)
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+/**
+	***argv = pointer to group of str 
+	char **argv = group of str
+	**argv = *argv[i]
+*/
 char	**process_quoted_arg(int *argc, char ***argv)
 {
 	char	**args_array;
 	char	**new_argv;
 	int		split_count;
 	int		i;
+	int		digit_check;
 
+	if (digit_check(argc, argv) == 0)
+		handle_error(NULL, NULL);
 	args_array = handle_string_input((*argv)[1]);
 	split_count = count_no_in_array(args_array);
 	new_argv = (char **)malloc((split_count + 2) * sizeof(char *));
